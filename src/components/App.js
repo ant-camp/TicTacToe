eb2b82dca19d0122e22980fb8079ad9c7f8560c0
Anthony <anttyaadiggg@yahoo.com>
2017-07-14 15:43:08 -0400

    start app

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Board from './Board';
+import Result from './Result';
+import './App.css';
+
+
+const App = () => {
+  return (
+    <div className="App">
+      <Board />
+      <Result />
+    </div>
+  );
+}
+
+export default App;
